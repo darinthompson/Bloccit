9962e94e8652b668a27f3ee676ab48d00549b383
darin.thompson <darint07@gmail>
2014-01-23 23:27:43 -0500

    First commit and README update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloccit::Application.config.secret_token = 'd2ae3005e2f86b027c1d59f9c705ab75d7f0ee951d43b4513d88f4fd326c0f7dd96c42ee2fd7dc93efc57ea534509393fa0c51b92d5af98e8f72d7623f978f43'
