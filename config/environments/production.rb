1fad4b65ac8b760ebd140b0dd557fbd3eba289df
darin.thompson <darint07@gmail>
2014-02-03 10:08:55 -0500

    update to action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,5 +66,4 @@ Bloccit::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   #http://bloccit-darinthompson.herokuapp.com
-  config.action_mailer.default_url_options = { :host => 'http://bloccit-darinthompson.herokuapp.com'}
-end
+  config.action_mailer.default_url_options = { :host => 'bloccit-darinthompson.herokuapp.com' }  end
