fc8b5d2659c532b0247abafba7e2dd8d375a892a
darin.thompson <darint07@gmail>
2014-02-03 15:51:30 -0500

    Installed and implemented Redcarper for markdown

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
- 
   def show
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
@@ -8,21 +7,23 @@ class PostsController < ApplicationController
   def new
     @topic = Topic.find(params[:topic_id])
     @post = Post.new
-    authorize! :create, @post, message: "You need to be a member to create a new post."  end
+    authorize! :create, Post, message: "You need to be a member to create a new post."
+  end
 
   def edit
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize! :edit, @post, message: "You need to own the post to edit it."
   end
-  
+
   def create
     @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(params[:post])
+    @post.topic = @topic
+
     authorize! :create, @post, message: "You need to be signed up to do that."
     if @post.save
-      flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [@topic, @post], notice: "Post was saved successfully."
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
@@ -30,15 +31,14 @@ class PostsController < ApplicationController
   end
 
   def update
-    @topic = Topic.find(params[:topic_ic])
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize! :update, @post, message: "You need to own the post to edit it."
     if @post.update_attributes(params[:post])
-      flash[:notice] = "Post was updated."
-      redirect_to @post
+      redirect_to [@topic, @post], notice: "Post was saved successfully."
     else
       flash[:error] = "There was an error saving the post. Please try again."
-      render :edit
+      render :new
     end
   end
 end
