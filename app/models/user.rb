78f0b7e3eed2b570750ced28f49c95feb9cc8cc4
darin.thompson <darint07@gmail>
2014-02-05 13:58:42 -0500

    Created yml. files for sensitive info and created ability to upload files.

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,9 +17,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :avatar
   # attr_accessible :title, :body
   has_many :posts
   before_create :set_member
+  mount_uploader :avatar, AvatarUploader #add this line
 end
 
