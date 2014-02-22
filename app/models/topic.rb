3efe2518b2137e88bc31c416e02f7600890f5e6e
darin.thompson <darint07@gmail>
2014-02-21 21:30:21 -0500

    Fixed error message

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ActiveRecord::Base
-  attr_accessible :description, :name, :public, :image
+  attr_accessible :description, :name, :public#, :image
   has_many :posts, dependent: :destroy
-  mount_uploader :image, ImageUploader
+#  mount_uploader :image, ImageUploader
 
   scope :visible_to, lambda { |user| user ? scoped : where(public: true) }
 end
