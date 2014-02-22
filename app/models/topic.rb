6ceb451c05976e4f1295d6cd61b5e3443f968398
darin.thompson <darint07@gmail>
2014-02-21 19:24:45 -0500

    I am lost

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,6 @@ class Topic < ActiveRecord::Base
   attr_accessible :description, :name, :public, :image
   has_many :posts, dependent: :destroy
   mount_uploader :image, ImageUploader
+
+  scope :visible_to, lambda { |user| user ? scoped : where(public: true) }
 end
